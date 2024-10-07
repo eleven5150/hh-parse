64c99225a2e35c183e1ec86c05ec116238fb5879
Kirill Kutaev <kkutaev@ptsecurity.com>
2024-10-07 23:43:49 +0300

    Separated to new tool

--- __main__.py
+++ __main__.py
@@ -1,52 +1,29 @@
 import argparse
-import json
 import sys
 
-import requests
-
-from vacancies import VacanciesQuery, Vacancy, Vacancies
+from vacancies import vacancies_subparser
 
 
 def parse_args(arguments: list):
     parser = argparse.ArgumentParser(description="hh-parse - tool for parsing hh.ru")
-    parser.add_argument("-s", "--search_query",
-                        type=str,
-                        required=True,
-                        help="Search query")
-    parser.add_argument("-a", "--areas",
-                        type=str,
-                        required=True,
-                        nargs="+",
-                        help="Area for search")
-    parser.add_argument("-r", "--roles",
-                        type=str,
-                        nargs="+",
-                        help="List of roles")
+
+    subparsers = parser.add_subparsers(
+        required=True,
+        title="tools",
+        help="Tool to use",
+        dest="tool",
+    )
+
+    vacancies_subparser(subparsers)
 
     return parser.parse_args(arguments)
 
 
 def main(raw_arguments: list) -> None:
     args = parse_args(raw_arguments[1:])
-    vacancies_query: VacanciesQuery = VacanciesQuery.args_to_vacancy_query(args)
-    vacancy_query_url: str = vacancies_query.get_url()
-    num_of_pages: int = json.loads(requests.get(vacancy_query_url).text)["pages"]
 
-    vacancies_list: list[Vacancy] = list()
-    for page_num in range(0, num_of_pages + 1):
-        page_data: any = json.loads(requests.get(vacancy_query_url, {"page": page_num}).text)
-        if "items" in page_data:
-            for raw_vacancy_short in page_data["items"]:
-                raw_vacancy_full: any = json.loads(
-                    requests.get(
-                        f"{vacancies_query.BASE_URL}{raw_vacancy_short['id']}"
-                    ).text
-                )
-                if "id" in raw_vacancy_full: # TODO: fix captcha
-                    vacancies_list.append(Vacancy.data_to_vacancy(raw_vacancy_full))
+    args.func(args)
 
-    vacancies: Vacancies = Vacancies(vacancies_list)
-    vacancies.to_csv(vacancies_query)
 
 
 if __name__ == '__main__':
--- vacancies.py
+++ vacancies.py
@@ -1,10 +1,30 @@
 import argparse
 import csv
+import json
+import requests
 
 from _csv import writer
 from dataclasses import dataclass
 from datetime import datetime
 from urllib.parse import urlencode
+
+def vacancies_subparser(subparser):
+    parser = subparser.add_parser("vacancies_tool", description="Tool for parsing vacancies")
+    parser.add_argument("-s", "--search_query",
+                        type=str,
+                        required=True,
+                        help="Search query")
+    parser.add_argument("-a", "--areas",
+                        type=str,
+                        required=True,
+                        nargs="+",
+                        help="Area for search")
+    parser.add_argument("-r", "--roles",
+                        type=str,
+                        nargs="+",
+                        help="List of roles")
+
+    return parser.set_defaults(func=tool_entrypoint)
 
 
 @dataclass
@@ -166,3 +186,25 @@ class Vacancies:
         ) as csv_file:
             csv_writer: writer = csv.writer(csv_file)
             csv_writer.writerows(table)
+
+
+def tool_entrypoint(args: argparse.Namespace) -> None:
+    vacancies_query: VacanciesQuery = VacanciesQuery.args_to_vacancy_query(args)
+    vacancy_query_url: str = vacancies_query.get_url()
+    num_of_pages: int = json.loads(requests.get(vacancy_query_url).text)["pages"]
+
+    vacancies_list: list[Vacancy] = list()
+    for page_num in range(0, num_of_pages + 1):
+        page_data: any = json.loads(requests.get(vacancy_query_url, {"page": page_num}).text)
+        if "items" in page_data:
+            for raw_vacancy_short in page_data["items"]:
+                raw_vacancy_full: any = json.loads(
+                    requests.get(
+                        f"{vacancies_query.BASE_URL}{raw_vacancy_short['id']}"
+                    ).text
+                )
+                if "id" in raw_vacancy_full:  # TODO: fix captcha
+                    vacancies_list.append(Vacancy.data_to_vacancy(raw_vacancy_full))
+
+    vacancies: Vacancies = Vacancies(vacancies_list)
+    vacancies.to_csv(vacancies_query)
